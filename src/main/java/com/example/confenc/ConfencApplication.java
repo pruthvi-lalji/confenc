942acae217f4d478d72e8378d4865afc8c8ce3f2
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-31 18:12:10 +0000

    Initial commit

---
+++ src/main/java/com/example/confenc/ConfencApplication.java
@@ -0,0 +1,14 @@
+package com.example.confenc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ConfencApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfencApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/confenc/ConfencApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confenc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfencApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
